501ad4817723e2656c9fa8ff775afdf5c6fefbb3
MatiassContreras <MatiHD4K@outlook.com.ar>
2022-10-06 10:25:18 -0300

    Funciones de calculos matematicos

--- script.js
+++ script.js
@@ -4,3 +4,13 @@ const VariableConst = "46177224";
 console.log(VariableLet)
 console.log(VariableVar)
 console.log(VariableConst)
+
+arraynumeros = (num1 = "45", num2 = "20")
+funcionsum = (num1 + num2)
+funcionrestar = (num1 - num2)
+funcionmultiplicar = (num1 * num2)
+funcionsdividir = (num1 / num2)
+console.log(funcionsum)
+console.log(funcionrestar)
+console.log(funcionmultiplicar)
+console.log(funcionsdividir)
